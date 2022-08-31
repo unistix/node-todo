352b7526a7549f88e54f8af87a23ea698d01b8dd
Cynny <c.dafinone@gmail.com>
2022-08-31 20:09:47 +0100

    remmoved comment

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-/*const connectionString = "mongodb+srv://john:1234@nodeexpressprojects.t642fwi.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority"*/
+
 
 
 const connectDB = (url) =>{
